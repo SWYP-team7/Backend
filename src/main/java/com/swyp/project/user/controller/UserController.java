fdd57ab6d956c8b2f49cdbb8c00427c96930c580
2jiyong <jdragon2000@naver.com>
2025-08-06 18:38:18 +0900

    chore: 스웨거 및 헬스체크 컨트롤러 임시 축

---
+++ src/main/java/com/swyp/project/user/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.swyp.project.user.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	@GetMapping("/health")
+	public ResponseEntity<String> healthCheck() {
+		return ResponseEntity.ok("OK");
+	}
+}
