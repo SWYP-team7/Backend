4dff5aa7ee75e028d0325bea6bb07054c38e69a5
2jiyong <jdragon2000@naver.com>
2025-08-07 21:48:02 +0900

    feat: 응답 구조 생성

---
+++ src/main/java/com/swyp/project/common/dto/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.swyp.project.common.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ApiResponse<T> {
+	private final boolean success;
+	private final T data;
+	private final ApiError error;
+
+	public static <T> ApiResponse<T> success(T data) {
+		return new ApiResponse<>(true, data, null);
+	}
+
+	public static <T> ApiResponse<T> success() {
+		return new ApiResponse<>(true, null, null);
+	}
+
+	public static <T> ApiResponse<T> failure(String message, String code) {
+		return new ApiResponse<>(false, null, new ApiError(message, code));
+	}
+}
