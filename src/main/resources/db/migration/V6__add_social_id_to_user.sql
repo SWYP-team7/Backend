282e8813ce0f3c36a02b9db4b1eae4279df0b8cd
2jiyong <jdragon2000@naver.com>
2025-09-03 00:57:24 +0900

    feat: 새로운 더미 유저 추가 및 기존 flyway 이름 수정

--- src/main/resources/db/migration/V6__add_social_id_to_user.sql
+++ src/main/resources/db/migration/V6__add_social_id_to_user.sql
@@ -1,5 +1,5 @@
--- V6_add_social_id_to_user.sql
+-- V6__add_social_id_to_user.sql
 
 -- user 테이블에 social id 컬럼 추가
-ALTER TABLE user
+ALTER TABLE users
 ADD COLUMN social_id BIGINT UNIQUE;
---
+++ src/main/resources/db/migration/V7__add_another_dummy_user.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO users
+(name, email, birthdate, created_at, updated_at, provider, provider_id, gender, profile_image_url, profile_completed, social_id)
+VALUES
+    ('더미 사용자2', 'dummy2.user@example.com', '1995-01-02', NOW(), NOW(), 'LOCAL', 'dev-002', 'MALE', 'https://picsum.photos/seed/dummy/20', 1, 1);
+
